f1621eb4f483bd5d22178c02be0d06de26ed9351
Anubhav Baghel <code.anubhavbaghel@gmail.com>
2026-04-08 00:58:53 +0530

    patched the marker icon

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,7 +1,19 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import useCurrentLocation from "../hooks/useCurrentLocation.js";
 import { useEffect, useState } from "react";
-import { marker } from "leaflet";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 export default function MapView() {
   const [events, setEvents] = useState([]);
