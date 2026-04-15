b6ce3a10ee0cdb706c20f283750f8b4f2c30669f
Anubhav Baghel <code.anubhavbaghel@gmail.com>
2026-04-16 01:33:25 +0530

    header text patch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ isleftSidebarOpen, setIsLeftSidebarOpen }) => {
   return (
     <div className="flex flex-row bg-white h-15 w-screen items-center gap-5 px-4 py-2 shadow-sm">
       <div>
-        <h1 className="text-black text-2xl p-2 rounded-lg hidden sm:block">
+        <h1 className="text-black text-2xl p-2 rounded-lg md:text-xl sm:block">
           What is happening in my City?
         </h1>
       </div>
